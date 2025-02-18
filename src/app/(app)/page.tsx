b98c31da4cfab3e49adbd3f5025cf6379d8cef5c
flvffywvffy <marcus@marcusleonasharvey.com>
2025-02-18 01:02:26 +0000

    add auth buttons to homepage

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,4 +1,5 @@
-import { auth } from "~/server/auth";
+import { Button } from "~/components/ui/button";
+import { auth, signOut } from "~/server/auth";
 
 export default async function HomePage() {
   const session = await auth();
@@ -9,14 +10,24 @@ export default async function HomePage() {
 
   return (
     <main className="px-24 py-8">
-      <div className="inline-flex items-center justify-between">
+      <div className="inline-flex w-full items-center justify-between">
         <div>
           <h1 className="text-4xl font-semibold">Command Catalogue</h1>
-          <p className="text-xl">
-            Welcome back, {session.user.name?.split(" ")[0]}.
-          </p>
+          <div className="inline-flex gap-1 text-xl">
+            <p>Welcome back, {session.user.name?.split(" ")[0]}.</p>
+            <form
+              action={async () => {
+                "use server";
+                await signOut();
+              }}
+            >
+              <button className="underline">Sign out</button>.
+            </form>
+          </div>
         </div>
-        <div></div>
+        <div>
+          <Button>Add New</Button>
+        </div>
       </div>
     </main>
   );
