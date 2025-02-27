ac433ba7525a36b50255a70b2ec09abf59237d00
flvffywvffy <marcus@marcusleonasharvey.com>
2025-02-27 01:51:53 +0000

    add plausible analytics

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,5 +1,6 @@
 import "~/styles/globals.css";
 
+import PlausibleProvider from "next-plausible";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 import AuthRedirect from "~/components/auth-redirect";
@@ -13,25 +14,30 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <AuthRedirect>
-      <html lang="en" className={`${GeistSans.variable}`}>
-        <body>
-          {children}
-          <footer className="px-4 py-2 text-sm md:px-24 md:py-8">
-            <p>
-              Commands Catalogue by Marcus Harvey.{" "}
-              <a
-                className="underline"
-                href="https://github.com/marcusleonas/command-catalogue"
-              >
-                Source Code
-              </a>
-              .
-            </p>
-          </footer>
-          <Toaster />
-        </body>
-      </html>
-    </AuthRedirect>
+    <PlausibleProvider
+      domain="commands.flvffy.top"
+      customDomain="https://plausible.flvffy.top/js/script.js"
+    >
+      <AuthRedirect>
+        <html lang="en" className={`${GeistSans.variable}`}>
+          <body>
+            {children}
+            <footer className="px-4 py-2 text-sm md:px-24 md:py-8">
+              <p>
+                Commands Catalogue by Marcus Harvey.{" "}
+                <a
+                  className="underline"
+                  href="https://github.com/marcusleonas/command-catalogue"
+                >
+                  Source Code
+                </a>
+                .
+              </p>
+            </footer>
+            <Toaster />
+          </body>
+        </html>
+      </AuthRedirect>
+    </PlausibleProvider>
   );
 }
